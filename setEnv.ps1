bfcb3cad957ed85115315d8ffba20e385e094910
SK <shawn.work.script@gmail.com>
2022-07-11 12:21:18 +0800

    Update info when load env.

--- setEnv.ps1
+++ setEnv.ps1
@@ -4,7 +4,9 @@ Write-Output "gproj - Create projects. param: [projType, dst]"
 Write-Output "pproj - Put project into production. param: [src, dst]"
 Write-Output "pwdman - Check Creds. param: [--mode {view, add}]"
 Write-Output "actconda - activate conda env in powershell"
+Write-Output "guide - Open guide with Chrome. param: [guideType]"
 Write-Output ""
+
 
 $Env:EDU = "D:\Env"
 $Env:PYWD = "C:\Users\shawn\PycharmProjects"
